3058ffb88788bb8be698f83c07b74392ff3ef361
George Rohn <Rohn2024@gmail.com>
2019-05-15 09:05:41 -0400

    fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ class Footer extends React.Component {
                         {/* <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li> */}
                         <li><a href="https://github.com/AlphaNumericPencil" className="icon fa-github"><span className="label">Github</span></a></li>
                         <li><a href="https://www.linkedin.com/in/george-rohn/" className="icon fa-linkedin"><span className="label">Dribbble</span></a></li>
-                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                        <li><a href="mailto:ContactDesignGrove@gmail.com" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
                     </ul>
                     <ul className="copyright">
                         <li>&copy; George Rohn</li>
