11cde4f884534b5ca8bf1992e8388e4a106ab21e
George Rohn <Rohn2024@gmail.com>
2019-06-03 18:00:04 -0400

    make it better

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,9 +21,13 @@ import full05 from '../assets/images/fulls/05.jpg'
 import full06 from '../assets/images/fulls/06.jpg'
 
 const DEFAULT_IMAGES = [
-    { id: '1', src: 'https://democratic-dj-app.herokuapp.com/', thumbnail: thumb03, caption: 'Democratic DJ', description: 'A collaborative playlist builder using the Spotify API and Next.js'},
-    { id: '2', src: 'https://georgerohn.neocities.org/#hero', thumbnail: thumb02, caption: 'RC Landscaping', description: 'A website built to showcase the work of a local landscaping company.'}
-  //  { id: '2', src: full02, thumbnail: thumb02, caption: 'Photo 2', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '1', src: 'https://pwa-talk.herokuapp.com/', thumbnail: thumb05, caption: 'Talk To Us', description: 'Under construction hosting platform for an up-and-coming political/social podcast run by friends and scholars.'},
+    { id: '1', src: 'https://abuse-cycle.herokuapp.com/', thumbnail: thumb04, caption: 'Abuse is a Cycle', description: 'Under construction hosting platform for a free resource aimed at arming the victims of violent abuse with the tools they need to escape the cycle.'},
+    { id: '3', src: 'https://democratic-dj-app.herokuapp.com/', thumbnail: thumb03, caption: 'Democratic DJ', description: 'A collaborative playlist builder using the Spotify API and Next.js'},
+    { id: '4', src: 'https://georgerohn.neocities.org/#hero', thumbnail: thumb02, caption: 'RC Landscaping', description: 'A website built to showcase the work of a local landscaping company.'}
+    
+  
+    //  { id: '2', src: full02, thumbnail: thumb02, caption: 'Photo 2', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
    // { id: '3', src: full03, thumbnail: thumb03, caption: 'Photo 3', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
     //{ id: '4', src: full04, thumbnail: thumb04, caption: 'Photo 4', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
     //{ id: '5', src: full05, thumbnail: thumb05, caption: 'Photo 5', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
